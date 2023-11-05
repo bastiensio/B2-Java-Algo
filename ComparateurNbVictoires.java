a1552a738782122a34d843c74f7ffb65fd16fcfa
bastiensio <119434267+bastiensio@users.noreply.github.com>
2023-11-05 19:58:13 +0100

    Add files via upload

---
+++ ComparateurNbVictoires.java
@@ -0,0 +1,17 @@
+package fr.gns.app;
+
+import java.util.Comparator;
+
+public class ComparateurNbVictoires implements Comparator<Joueur> {
+
+	@Override
+	public int compare(Joueur o1, Joueur o2) {
+		    if (o1.getNbVictoires() > o2.getNbVictoires()) {
+		        return 1;
+		    } else if (o1.getNbVictoires() == o2.getNbVictoires()) {
+		        return 0;
+		    } else {
+		        return -1;
+		    }
+		}
+	}
